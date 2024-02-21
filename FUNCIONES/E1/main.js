b49a1a061eb8ec5d12b6421a5797be45bc31bbb6
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-02-21 21:44:24 +0100

    TERMINADO/

--- FUNCIONES/E1/main.js
+++ FUNCIONES/E1/main.js
@@ -1,13 +1,17 @@
+function quantity(){
+ let a = parseInt(prompt("Cuantas veces queres que te pregunte (maximo 5 veces):"));
+ answers(a);
+}
 
-function answers(){
-    for(let i=1; i<=5; i++){
+function answers(a){
+    for(let i=1; i<=a; i++){
     let salute = prompt("Di un saludo:");
     let name = prompt("Di un nombre: ");
     document.write(i + " - " + salute + ", " + name);
-    document.write("</br>")
+    document.write("</br>");
 
     }
 }
 
 
-answers();
+quantity();
