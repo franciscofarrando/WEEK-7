b76def2c9bac41fe777d7581d5da1a767ab13a7f
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-02-22 18:18:45 +0100

    funcionando!

--- FUNCIONES/E2/main.js
+++ FUNCIONES/E2/main.js
@@ -3,18 +3,18 @@ function questions(){
     let number2 = parseInt(prompt("Elegi otro numero"));
     let action = prompt("Quieres sumar o restar? + o -");
 
-        if (action == "sumar" && action == "+"){
+        if (action == "sumar" || action == "+"){
         let suma = (number1+number2);
         
         document.write("Tu suma es: " + number1  + " + " + number2 + " = " + suma);
         
-        }else if (action == "restar" ||action == "-"){
+        }else if (action == "restar" || action == "-"){
             let resta = (number1-number2);
             
             document.write("Tu resta es: " + number1  + " - " + number2 + " = " + resta);
             
         }else{
-            document.write("Ha habido un error")
+            document.write("Ha habido un error");
         }
 }
 
