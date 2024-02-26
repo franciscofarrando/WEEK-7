a490ae532133bf3c3591d92ef7256c3d47399d6c
franciscofarrando <121203370+franciscofarrando@users.noreply.github.com>
2024-02-26 18:38:59 +0100

    var a let modificado

--- DOM/E2/main.js
+++ DOM/E2/main.js
@@ -1,9 +1,9 @@
 function infoCaption(){
     let name = document.getElementById("name").value;
     let sirname = document.getElementById("sirname").value;
-    var checkboxes = document.getElementsByName('seat');
-    var selectedSeats = [];
-    for (var i = 0; i < checkboxes.length; i++) {
+    let checkboxes = document.getElementsByName('seat');
+    let selectedSeats = [];
+    for (let i = 0; i < checkboxes.length; i++) {
         if (checkboxes[i].checked) {
             selectedSeats.push(checkboxes[i].value);
         }
@@ -12,7 +12,7 @@ function infoCaption(){
     document.getElementById("resultName").innerHTML = name;
     document.getElementById("resultSN").innerHTML = sirname;
 
-    var resultDiv = document.getElementById('resultSelection');
+    let resultDiv = document.getElementById('resultSelection');
     resultDiv.innerHTML = selectedSeats.join(', ');
     
 }
